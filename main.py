465a8d05a18284618caaa24a04e76a2c29234258
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-03-17 13:58:55 -0500

    save the model

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ from keras import preprocessing
 #Code starts here
 data = keras.datasets.imdb
 
-(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=10000)
+(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=88000)
 
 # print(train_data[0])
 # data with different length => can't come up with the shape needed for our neural network model
-print(len(train_data[0]), len(train_data[1]))
+# print(len(train_data[0]), len(train_data[1]))
 word_index = data.get_word_index()
 
 word_index = {k: (v+3) for k, v in word_index.items()}
@@ -26,7 +26,7 @@ reverse_word_index = dict([(value, key) for(key, value) in word_index.items()])
 # Preprocess data by capping data length at 250 and for data with length less than 250, the data will be filled with 0
 train_data = preprocessing.sequence.pad_sequences(train_data, value=word_index["<PAD>"], padding="post", maxlen =250)
 test_data = preprocessing.sequence.pad_sequences(test_data, value=word_index["<PAD>"], padding="post", maxlen =250)
-print(len(train_data[0]), len(train_data[1]))
+# print(len(train_data[0]), len(train_data[1]))
 
 def decode_review(text):
     return " ".join([reverse_word_index.get(i, "?") for i in text])
@@ -37,7 +37,7 @@ def decode_review(text):
 
 # Set up model
 model = keras.Sequential()
-model.add(keras.layers.Embedding(10000, 16))
+model.add(keras.layers.Embedding(88000, 16))
 model.add(keras.layers.GlobalAveragePooling1D())
 model.add(keras.layers.Dense(16, activation = "relu"))
 model.add(keras.layers.Dense(1, activation = "sigmoid"))
@@ -57,11 +57,17 @@ result = model.evaluate(test_data, test_labels)
 
 print(result)
 
-class_name = ["negative", "positive"]
+# Save the model
+model.save('imdb.h5')
 
-test_review = test_data[1]
-prediction = model.predict([test_review])
-# print(prediction)
-print("Review:\n", decode_review(test_review))
-print("Prediction:\n", class_name[round(prediction[0][0])])
-print("Actual:\n", class_name[test_labels[1]])
+
+
+
+# class_name = ["negative", "positive"]
+#
+# test_review = test_data[2]
+# prediction = model.predict([test_review])
+# # print(prediction)
+# print("Review:\n", decode_review(test_review))
+# print("Prediction:\n", class_name[round(prediction[0][0])])
+# print("Actual:\n", class_name[test_labels[2]])
